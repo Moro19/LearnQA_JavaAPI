90a41cb003cb8da1ea7d91d82855dc0531185d96
Moro19 <amaru89@yandex.ru>
2024-12-26 14:50:01 +0300

    комит боли

--- src/test/java/LongRedirect.java
+++ src/test/java/LongRedirect.java
@@ -2,65 +2,35 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
-
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-
 
 public class LongRedirect {
-  @Test
-    public void testRestAssured() {
-     int count = 0;
-   //  int statusCode=0;
+    @Test
+    public void testLongRedirect() {
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int counter = 0;
+        int statusCode = 0;
 
-       // for (int i=0;i<size;i++) {
-      Response  response = RestAssured
-                  .given()
-                  .redirects()
-                 .follow(false)
-                  .when()
-                  .get("https://playground.learnqa.ru/api/long_redirect")
-                  .andReturn();
-
-        // count++;
-  //  List<String> jresponse = response.jsonPath().getList("$");
-   // for (int i = 0; i < jsonResponse.size(); i++) {
+        while (statusCode != 200) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+            counter++;
 
 
-      int statusCode =response.getStatusCode();
-      response.print();
-     String locationHeader=response.getHeader("Location");
-    // System.out.println(locationHeader);
-
-    List<Object> head = new ArrayList<>();
-    head.add(statusCode);
-if (statusCode !=200) {
-  get("https://playground.learnqa.ru/api/long_redirect");
-  count++;
-}
-else {
-  System.out.println(locationHeader);
-}
-}
-
-
-
-  /*  while (statusCode !=200) {
-      Response  response2= given()
-              .redirects()
-              .follow(true)
-              .when()
-              .get("https://playground.learnqa.ru/")
-              .andReturn();
-      ++count;
+            statusCode = response.getStatusCode();
+            url = response.getHeader("Location");
+        }
+        System.out.println("Количество редиректов: " + counter);
     }
 
-    System.out.println(count); */
+}
 
 
-  }
+
 
 
 
